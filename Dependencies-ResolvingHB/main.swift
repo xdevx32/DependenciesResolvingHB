2de1215fc48dfd3ff877f144f0867b1c96cd005e
xdevx32 <angelkukushev333@gmail.com>
2015-11-01 15:00:10 +0200

    Initial Commit

---
+++ Dependencies-ResolvingHB/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Dependencies-ResolvingHB
+//
+//  Created by Angel Kukushev on 11/1/15.
+//  Copyright © 2015 xdevx332. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
